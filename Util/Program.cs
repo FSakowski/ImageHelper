5dfbb148b5e0e45343d32043b231a676bd05340b
Florian Sakowski <florian.sakowski@googlemail.com>
2019-12-11 22:22:38 +0100

    test tool updated

--- Util/Program.cs
+++ Util/Program.cs
@@ -11,12 +11,14 @@ namespace Util
     {
         static void Main(string[] args)
         {
-            ImageProcessor processor = ImageProcessor.FromFile(@"D:\Pictures\Sync\2019\09\IMG_1149.JPG");
-            processor.Add(Grinder.MakeRound(1, 50));
-            processor.Add(ColorAdaption.Decolorize());
-            processor.Add(ColorAdaption.Lighten(-30));
-            processor.SaveToFile(@"C:\Users\flo\Pictures\rounded.png");
-
+            using (ImageProcessor processor = ImageProcessor.FromFile(@"D:\Pictures\Sync\2019\09\IMG_1149.JPG"))
+            {
+                processor.Add(Grinder.MakeRound(1, 50));
+                processor.Add(ColorAdaption.Decolorize());
+                processor.Add(ColorAdaption.Lighten(-30));
+                processor.Add(ColorAdaption.Invert());
+                processor.SaveToFile(@"C:\Users\flo\Pictures\rounded.png");
+            }
         }
     }
 }
